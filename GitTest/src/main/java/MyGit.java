4bd8fbc84129ac9ad74e547462ab69736c4cab00
shuai <2532780436@qq.com>
2022-10-18 18:11:18 +0800

    初始化工程~

---
+++ GitTest/src/main/java/MyGit.java
@@ -0,0 +1,19 @@
+
+public class MyGit {
+    private String name;
+
+    @Override
+    public String toString() {
+        return "MyGit{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
