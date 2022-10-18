385e7143306baacf95db532bc7be15a598b0d121
shuai <2532780436@qq.com>
2022-10-18 19:17:42 +0800

    老油条第一次修改代码

--- GitTest/src/main/java/MyGit.java
+++ GitTest/src/main/java/MyGit.java
@@ -1,7 +1,15 @@
 
 public class MyGit {
     private String name;
+    private String password;
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     @Override
     public String toString() {
