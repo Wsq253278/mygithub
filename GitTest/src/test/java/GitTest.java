8c836b2f39749cfa52316b66540cf2f56b73dd25
shuai <2532780436@qq.com>
2022-10-18 18:35:50 +0800

    第一次热修复

--- GitTest/src/test/java/GitTest.java
+++ GitTest/src/test/java/GitTest.java
@@ -1,5 +1,8 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("入职第一天");
+        System.out.println("进行热修复。。。");
+        System.out.println("又来了");
     }
+
 }
