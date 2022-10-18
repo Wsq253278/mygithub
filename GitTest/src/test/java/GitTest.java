265f447cd31a7f60505985c730ccceed37eb1e4e
shuai <2532780436@qq.com>
2022-10-18 18:40:58 +0800

    master主分支

--- GitTest/src/test/java/GitTest.java
+++ GitTest/src/test/java/GitTest.java
@@ -3,6 +3,7 @@ public class GitTest {
         System.out.println("入职第一天");
         System.out.println("进行热修复。。。");
         System.out.println("又来了");
+        System.out.println("hello master");
     }
 
 }
